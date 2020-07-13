5d78000164fa48d8eb5ea9d255c4e5979c93b6a4
dbsrlwo1 <dbsrlwo1@naver.com>
2020-07-13 16:29:14 +0900

    API 불러 와서 지도 뿌리기 성공

--- 0708_kakaoTest/app/src/main/java/com/example/a0708_kakaotest/Android_Class/Init_Calss/Init_Data.java
+++ 0708_kakaoTest/app/src/main/java/com/example/a0708_kakaotest/Android_Class/Init_Calss/Init_Data.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+import android.widget.Toast;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.opencsv.CSVReader;
@@ -15,12 +17,12 @@ import java.util.List;
 
 public class Init_Data {
 
-    private static List<String[]> csv;
+    private static List<String[]> csv_bank;
     public Init_Data(Activity ac){
         prepArray(ac);
     }
-    public static List<String[]> getData(){
-        return csv;
+    public static List<String[]> get_bankData(){
+        return csv_bank;
     }
     private void prepArray(Activity ac) {
         try {
@@ -34,10 +36,10 @@ public class Init_Data {
             }
             AssetManager am = ac.getResources().getAssets() ;
             InputStream csvStream = am.open("BankStandard_data.csv");
-            //Toast.makeText(this, "공공데이터를 불러오는중..", Toast.LENGTH_SHORT).show();
-            InputStreamReader reader = new InputStreamReader(csvStream, Charset.forName("UTF-8"));
-            csv = new CSVReader(reader).readAll();
-            //Toast.makeText(this, "공공데이터를 업로드중..", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(ac, "공공데이터를 불러오는중..", Toast.LENGTH_SHORT).show();
+            InputStreamReader reader = new InputStreamReader(csvStream, Charset.forName("x-windows-949"));
+            csv_bank = new CSVReader(reader).readAll();
+            Toast.makeText(ac, "공공데이터를 업로드중..", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
         }
