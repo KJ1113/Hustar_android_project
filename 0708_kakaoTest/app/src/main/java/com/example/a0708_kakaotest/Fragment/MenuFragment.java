96bac4f5b1974e1d521308ba508a7590ce506d61
KJ_1113 <dbsrlwo1@gmail.com>
2020-07-13 19:55:18 +0900

    집에서

--- 0708_kakaoTest/app/src/main/java/com/example/a0708_kakaotest/Fragment/MenuFragment.java
+++ 0708_kakaoTest/app/src/main/java/com/example/a0708_kakaotest/Fragment/MenuFragment.java
@@ -1,16 +1,33 @@
 package com.example.a0708_kakaotest.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
 import com.example.a0708_kakaotest.Fragment.MenuFragment_leaf_Fragments.*;
 import com.example.a0708_kakaotest.R;
 
@@ -21,46 +38,50 @@ import java.util.List;
 public class MenuFragment extends Fragment {
 
     private View view;
-    private ViewPager mViewPager;
-    private PagerAdapter pagerAdapter;
+    private Animation animation;
+    private ViewPager viewPager;
+    private FrameLayout in_fr;
+    ViewTreeObserver mGlobalLayoutListener;
 
 
-
-    private Leaf_Fragment_1 leaf_Fragment_1 = new Leaf_Fragment_1();
-    private Leaf_Fragment_2 leaf_Fragment_2 = new Leaf_Fragment_2();
-    private Leaf_Fragment_3 leaf_Fragment_3 = new Leaf_Fragment_3();
+    float downX;
+    float upX;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_menu, container, false);
-        mViewPager = (ViewPager)view.findViewById(R.id.mViewPager);
-        pagerAdapter  = new PagerAdapter(getChildFragmentManager());
 
-        setupViewPager(mViewPager);
         return view;
     }
-    public void setupViewPager(ViewPager viewPager) {
-        pagerAdapter.addFragment(leaf_Fragment_1);
-        pagerAdapter.addFragment(leaf_Fragment_2);
-        pagerAdapter.addFragment(leaf_Fragment_3);
-        viewPager.setAdapter(pagerAdapter);
-    }
-    public class PagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        public void addFragment(Fragment fragment){
-            mFragmentList.add(fragment);
+
+    public class MeasuredViewPager extends ViewPager {
+
+
+        public MeasuredViewPager(Context context) {
+            super(context);
         }
-        public PagerAdapter(FragmentManager fm){
-            super(fm);
+
+        public MeasuredViewPager(Context context, AttributeSet attrs) {
+            super(context, attrs);
         }
+
         @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            int mode = MeasureSpec.getMode(heightMeasureSpec);
+            if (mode == MeasureSpec.UNSPECIFIED || mode == MeasureSpec.AT_MOST) {
+                super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+                int height = 0;
+                for (int i = 0; i < getChildCount(); i++) {
+                    View child = getChildAt(i);
+                    child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                    int h = child.getMeasuredHeight();
+                    if (h > height) height = h;
+                }
+                heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+            }
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
-        @Override
-        public int getCount() {
-            return mFragmentList.size() ;
-        }
+
     }
 }
