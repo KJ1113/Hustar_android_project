5f152172a01de453522d08ae0a90565b2378f20b
DESKTOP-Q7VRKV5\YoonGiJae <dbsrlwo1@gmail.com>
2020-11-24 17:20:37 +0900

    실시간 위치, 나침반 추가

--- 0708_kakaoTest/app/src/main/java/com/example/a0708_kakaotest/Android_Class/menu_FragmentUse_Class/ViewHolderPage.java
+++ 0708_kakaoTest/app/src/main/java/com/example/a0708_kakaotest/Android_Class/menu_FragmentUse_Class/ViewHolderPage.java
@@ -2,7 +2,6 @@ package com.example.a0708_kakaotest.Android_Class.menu_FragmentUse_Class;
 
 import android.view.View;
 import android.widget.ImageView;
-
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.a0708_kakaotest.R;
